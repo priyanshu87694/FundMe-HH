6814bc1f7806ed99a2b8e780d68e53d13a627966
priyanshu87694 <priyanshu87694@gmail.com>
2022-06-27 19:59:07 +0530

    deploying done locally and on testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,22 +5,12 @@ require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "0x00"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x00"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x00"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x00"
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   // solidity: "0.8.8",
   solidity: {
@@ -31,15 +21,21 @@ module.exports = {
   },
   defaultNetwork: "hardhat",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      // rinkeby chainId
+      chainId: 4,
+      blockConfirmation: 6,
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "INR",
+    coinmarketCap: COINMARKETCAP_API_KEY,
+    // token: "MATIC",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
