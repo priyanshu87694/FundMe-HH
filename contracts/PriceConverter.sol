73c5b00ab96e72f03820c2fa967147c55db05b8c
priyanshu87694 <priyanshu87694@gmail.com>
2022-06-27 18:56:18 +0530

    Changed  to  PriceConverter

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -5,13 +5,12 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice (AggregatorV3Interface priceFeed) public view returns(uint256) {
-        // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (, int price, , , ) = priceFeed.latestRoundData();
+    function getPrice (AggregatorV3Interface priceFeed) internal view returns(uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
         return uint256(price * 1e10);
     }
 
-    function getConversionRate (uint256 amount, AggregatorV3Interface priceFeed) public view returns (uint256) {
+    function getConversionRate (uint256 amount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
         uint256 price = getPrice(priceFeed);
         return (amount * price) / 1e18;
     }
